8df3f2ff07460b0ada9fa87e2a654758ae36606c
PAUL HARDMAN <89204135+singapaul@users.noreply.github.com>
2023-05-25 07:29:23 +0100

    Using forEach to avoid concurrency issue

--- uploadEntry.js
+++ uploadEntry.js
@@ -1,16 +1,11 @@
 const contentful = require("contentful-management");
 const results = require("./output/file");
 
-const greet = () => {
-  console.log("delay waited");
-};
-
 const client = contentful.createClient({
   accessToken: "_6eQ_KaRxREC685Bq56csw-gt4b8aG0NWhLr3e668Go",
 });
 
 const upLoadEntry = async (entry) => {
-  setTimeout(greet, 5000);
   await client
     .getSpace("clau520o6xa1")
     .then((space) => space.getEnvironment("master-2023-05-24"))
@@ -37,10 +32,36 @@ const upLoadEntry = async (entry) => {
     )
     .then((entry) => console.log(entry))
     .catch(console.error);
-  setTimeout(greet, 5000);
 };
 
-results.forEach((element) => {
-  console.log(element);
-  upLoadEntry(element);
-});
+function resolveAfter2Seconds(entry) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("resolved");
+      upLoadEntry(entry);
+    }, 2000);
+  });
+}
+
+async function asyncCall(entry) {
+  console.log("calling");
+  const result = await resolveAfter2Seconds(entry);
+  console.log(result);
+  // Expected output: "resolved"
+}
+
+// Using forEach with an asynchronous function can lead to
+//  unexpected results or incorrect behavior.
+//  The forEach method does not work well
+//  with asynchronous operations because it does not wait for
+//  the completion of each iteration before moving on to the next one
+//  . This can cause concurrency issues and incorrect sequencing
+//  of async operations.
+
+const finalFunc = async (results) => {
+  for (const item of results) {
+    await asyncCall(item);
+  }
+};
+
+finalFunc(results);
